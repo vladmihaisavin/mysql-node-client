0fa5718c609c8123cce1ffdd551d60de8ab45c74
Vlad-Mihai Savin <savin.vladmihai@gmail.com>
2020-05-07 13:02:01 +0300

    fixed script for local-cold-start

--- scripts/local-cold-start-mysql-server.sh
+++ scripts/local-cold-start-mysql-server.sh
@@ -1,12 +1,14 @@
-docker run -p 3307:3306 -p 33060:33060 --name=mysql-server-instance --mount type=bind,source="$(pwd)"/scripts,target=/app/scripts -d mysql/mysql-server
-echo ""
-echo "Booting up mysql server..."
-sleep 30
-PASS=$(docker logs mysql-server-instance 2>&1 | grep GENERATED | sed -n 's/.*PASSWORD: //p')
-docker exec -it mysql-server-instance mysql -u root -p$PASS
+#!/usr/bin/env bash
+# docker run -p 3307:3306 -p 33060:33060 --name=mysql-server-instance --mount type=bind,source="$(pwd)"/scripts,target=/app/scripts -d mysql/mysql-server
 
-# COMMAND="mysql -u root -p$PASS < /app/scripts/docker_config.sql"
-# docker exec mysql-server-instance /bin/sh -c "$COMMAND"
+echo "Booting up mysql server..."
+. "$(pwd)"/scripts/wait-for-container.sh mysql-server-instance 120
+
+PASS=$(docker logs mysql-server-instance 2>&1 | grep GENERATED | sed -n 's/.*PASSWORD: //p')
+# docker exec -it mysql-server-instance mysql -u root -p$PASS
+# docker exec -it mysql-server-instance /bin/sh
+
+docker exec mysql-server-instance mysql -u root -p$PASS -e "source /app/scripts/docker_config.sql"
 
 # SET_ALIAS="mysql_config_editor set --login-path=myAlias --host=localhost --user=root --password=$PASS"
 # docker exec mysql-server-instance /bin/sh -c "$SET_ALIAS"
