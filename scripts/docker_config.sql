fb2e73b534ebd18e295ac606b6b7443b87c0aa7c
Vlad-Mihai Savin <savin.vladmihai@gmail.com>
2020-05-18 17:30:01 +0300

    fixed docker_config.sql

--- scripts/docker_config.sql
+++ scripts/docker_config.sql
@@ -1,4 +1,4 @@
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'asd123';
 CREATE USER 'myUser'@'172.17.0.1' IDENTIFIED WITH mysql_native_password BY 'asd123';
 GRANT ALL PRIVILEGES ON *.* TO 'myUser'@'172.17.0.1';
-CREATE DATABASE test_db
+CREATE DATABASE test_db;
