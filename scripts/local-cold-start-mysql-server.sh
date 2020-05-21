a46d48763e0dadafcfa2ca953c2736fda29729c9
Vlad Savin <vlad.savin@acuris.com>
2020-05-21 10:17:57 +0300

    updated local-cold-start script to pass password

--- scripts/local-cold-start-mysql-server.sh
+++ scripts/local-cold-start-mysql-server.sh
@@ -8,7 +8,7 @@ PASS=$(docker logs mysql-server-instance 2>&1 | grep GENERATED | sed -n 's/.*PAS
 # docker exec -it mysql-server-instance mysql -u root -p$PASS
 # docker exec -it mysql-server-instance /bin/sh
 
-docker exec mysql-server-instance mysql -u root -p$PASS -e "source /app/scripts/docker_config.sql"
+docker exec mysql-server-instance mysql -u root -p$PASS -e "source /app/scripts/docker_config.sql" --connect-expired-password
 
 # SET_ALIAS="mysql_config_editor set --login-path=myAlias --host=localhost --user=root --password=$PASS"
 # docker exec mysql-server-instance /bin/sh -c "$SET_ALIAS"
