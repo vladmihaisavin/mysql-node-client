08dc4833b20c552856d94a4dae2702a707474b10
Vlad Savin <vlad.savin@acuris.com>
2020-06-18 17:34:40 +0300

    cleaned up startup scripts

--- scripts/local-cold-start-mysql-server.sh
+++ scripts/local-cold-start-mysql-server.sh
@@ -5,10 +5,4 @@ echo "Booting up mysql server..."
 . "$(pwd)"/scripts/wait-for-container.sh mysql-server-instance 120
 
 PASS=$(docker logs mysql-server-instance 2>&1 | grep GENERATED | sed -n 's/.*PASSWORD: //p')
-# docker exec -it mysql-server-instance mysql -u root -p$PASS
-# docker exec -it mysql-server-instance /bin/sh
-
 docker exec mysql-server-instance mysql -u root -p$PASS -e "source /app/scripts/docker_config.sql" --connect-expired-password
-
-# SET_ALIAS="mysql_config_editor set --login-path=myAlias --host=localhost --user=root --password=$PASS"
-# docker exec mysql-server-instance /bin/sh -c "$SET_ALIAS"
