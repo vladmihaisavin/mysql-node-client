e71afd11638b577706ee57d012c38cd2a077c497
Vlad Savin <vlad.savin@acuris.com>
2020-07-02 12:22:55 +0300

    updated db structure

--- scripts/docker_config.sql
+++ scripts/docker_config.sql
@@ -8,23 +8,31 @@ CREATE TABLE IF NOT EXISTS users (
   type VARCHAR(255) DEFAULT "guest",
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255),
+  password VARCHAR(255) NOT NULL,
+  createdAt TIMESTAMP,
+  updatedAt TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS book_types (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
   createdAt TIMESTAMP,
   updatedAt TIMESTAMP
 );
 CREATE TABLE IF NOT EXISTS books (
   id INT AUTO_INCREMENT PRIMARY KEY,
+  book_type_id INT,
   name VARCHAR(255) NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  pages INT NOT NULL,
+  description VARCHAR(255),
+  pages INT,
   createdAt TIMESTAMP,
-  updatedAt TIMESTAMP
+  updatedAt TIMESTAMP,
+  FOREIGN KEY (book_type_id) REFERENCES book_types(id)
 );
 CREATE TABLE IF NOT EXISTS authors (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
   birthday VARCHAR(255),
   typeOfAuthor VARCHAR(255),
   createdAt TIMESTAMP,
@@ -38,12 +46,3 @@ CREATE TABLE IF NOT EXISTS authors_books (
   FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE CASCADE,
   FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
 );
-CREATE TABLE IF NOT EXISTS book_types (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  createdAt TIMESTAMP,
-  updatedAt TIMESTAMP
-);
-ALTER TABLE books ADD book_type_id INT;
-ALTER TABLE books ADD FOREIGN KEY (book_type_id) REFERENCES book_types(id);
